65613c30eb1cd1090ad3bc9e1adedb31a19e8549
ianmottert <motterti@gmail>
2021-08-27 12:57:11 -0500

    validated the input for bonus missions

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -3,12 +3,30 @@ package org.launchcode.java.studios.areaofacircle;
 import java.util.Scanner;
 
 public class Area {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         double radius;
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter a radius: ");
-        radius = input.nextDouble();
-        double area = Circle.getArea(radius);
-        System.out.println("The area of a circle of radius " + radius + " is " + area + ".");
+        boolean verified = false;
+
+        while (!verified) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter a radius: ");
+            if (input.hasNextDouble()) {
+                radius = input.nextDouble();
+                if (radius > 0) {
+                    verified = true;
+                    double area = Circle.getArea(radius);
+                    System.out.println("The area of a circle of radius " + radius + " is " + area + ".");
+                } else {
+                    throw new Exception("Must be a positive number.");
+                }
+            } else {
+                throw new Exception("Must be a number.");
+            }
+        }
+
+
+
+
+
     }
 }
