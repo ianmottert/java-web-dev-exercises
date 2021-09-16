3a9d737a27ae2f1ed8a1d4e2dc700649b1d728c9
ianmottert <motterti@gmail>
2021-09-16 16:27:08 -0500

    Unit Testing exercises done

---
+++ src/org/launchcode/java/restaurantMenuStudio/Menu.java
@@ -0,0 +1,23 @@
+package org.launchcode.java.restaurantMenuStudio;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Menu {
+    private String dateUpdated;
+    ArrayList<MenuItem> foodItems = new ArrayList<>();
+
+    // constructor
+    public Menu(String dateUpdated) {
+        this.dateUpdated = dateUpdated;
+    }
+
+    void addItem() {
+
+    }
+
+    private void lastUpdated() {
+
+    }
+
+}
